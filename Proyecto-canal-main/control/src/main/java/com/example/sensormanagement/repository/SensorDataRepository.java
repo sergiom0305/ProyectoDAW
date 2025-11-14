2d17c8468946f6b8cbecd7aa23ab1b15f9716690
sergiom0305 <128742323+sergiom0305@users.noreply.github.com>
2025-11-13 19:21:33 -0500

    Delete Proyecto-canal-main directory

--- Proyecto-canal-main/control/src/main/java/com/example/sensormanagement/repository/SensorDataRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sensormanagement.repository;
-
-import com.example.sensormanagement.model.SensorData;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface SensorDataRepository extends JpaRepository<SensorData, Long> {
-}
